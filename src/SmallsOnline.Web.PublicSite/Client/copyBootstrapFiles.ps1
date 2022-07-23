59c6636391a1f32b3b610ebec591aa0f86743fc3
Timothy Small <smalls.online@icloud.com>
2022-07-23 19:14:43 -0400

    Added highlight.js copy

--- src/SmallsOnline.Web.PublicSite/Client/copyBootstrapFiles.ps1
+++ src/SmallsOnline.Web.PublicSite/Client/copyBootstrapFiles.ps1
@@ -15,6 +15,11 @@ $bootstrapIconsCssPath = Join-Path -Path $scriptRoot -ChildPath "node_modules\bo
 $bootstrapIconsFontDirPath = Join-Path -Path $scriptRoot -ChildPath "node_modules\bootstrap-icons\font\fonts\"
 $bootstrapIconsOutPath = Join-Path -Path $scriptRoot -ChildPath "wwwroot\css\bootstrap-icons\"
 
+$highlightJsPath = Join-Path -Path $scriptRoot -ChildPath "node_modules\@highlightjs\cdn-assets\highlight.min.js"
+$highlightJsCssPath = Join-Path -Path $scriptRoot -ChildPath "node_modules\@highlightjs\cdn-assets\styles\github.min.css"
+$highlightJsOutPath = Join-Path -Path $scriptRoot -ChildPath "wwwroot\highlight.js\"
+$highlightJsCssOutPath = Join-Path -Path $scriptRoot -ChildPath "wwwroot\highlight.js\styles\"
+
 if (!(Test-Path -Path $bootstrapOutPath)) {
     $null = New-Item -Path $bootstrapOutPath -ItemType "Directory"
 }
@@ -44,3 +49,16 @@ Copy-Item -Path $bootstrapIconsCssPath -Destination $bootstrapIconsOutPath -Erro
 
 Write-Information @writeInfoSplat -MessageData "`t| fonts\-> $($bootstrapIconsOutPath)"
 Copy-Item -Path $bootstrapIconsFontDirPath -Destination $bootstrapIconsOutPath -Recurse -ErrorAction "Stop"
+
+if (Test-Path -Path $highlightJsOutPath) {
+    Remove-Item -Path $highlightJsOutPath -Recurse -Force
+}
+
+$null = New-Item -Path $highlightJsOutPath -ItemType "Directory"
+$null = New-Item -Path $highlightJsCssOutPath -ItemType "Directory"
+
+Write-Information @writeInfoSplat -MessageData "`t| highlight.min.js-> $($highlightJsOutPath)"
+Copy-Item -Path $highlightJsPath -Destination $highlightJsOutPath -ErrorAction "Stop"
+
+Write-Information @writeInfoSplat -MessageData "`t| styles\github.min.css-> $($highlightJsCssOutPath)"
+Copy-Item -Path $highlightJsCssPath -Destination $highlightJsCssOutPath -Recurse -ErrorAction "Stop"
