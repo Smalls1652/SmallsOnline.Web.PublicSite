77835f1f79a6679207065bb1934812690602d831
Timothy Small <smalls.online@icloud.com>
2022-09-20 13:17:48 -0400

    Changed logging to throw an exception
    This will help with the logic on the C# side since I can run it through
    a proper try/catch scenario.

--- src/SmallsOnline.Web.PublicSite/Client/Shared/MainLayout.razor.js
+++ src/SmallsOnline.Web.PublicSite/Client/Shared/MainLayout.razor.js
@@ -2,7 +2,7 @@ export const scrollToAnchorId = (anchorId) => {
     let foundElement = document.getElementById(anchorId);
 
     if (foundElement == null) {
-        console.log(`Could not find an element with the ID named '${anchorId}'.`);
+        throw `Could not find an element with the ID named '${anchorId}'.`;
     }
     else {
         foundElement.scrollIntoView();
