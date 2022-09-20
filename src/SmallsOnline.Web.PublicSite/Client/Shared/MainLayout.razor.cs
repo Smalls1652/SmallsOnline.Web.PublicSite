b28362daccec9661be92c984d49385e8316acc09
Timothy Small <smalls.online@icloud.com>
2022-09-20 13:45:34 -0400

    Added comments to anchor scrolling methods

--- src/SmallsOnline.Web.PublicSite/Client/Shared/MainLayout.razor.cs
+++ src/SmallsOnline.Web.PublicSite/Client/Shared/MainLayout.razor.cs
@@ -50,38 +50,69 @@ public partial class MainLayout : LayoutComponentBase, IDisposable
         _isEnableFadeSlideALocationChangeEventMethod = false;
     }
 
+    /// <summary>
+    /// Handles scrolling to an element on location change events.
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="eventArgs"></param>
     private async void ScrollToAnchorOnLocationChange(object? sender, LocationChangedEventArgs eventArgs)
     {
         await ScrollToAnchorAsync(eventArgs.Location);
     }
 
+    /// <summary>
+    /// Initiate a scroll to an element.
+    /// </summary>
+    /// <param name="inputLocation">The current Uri.</param>
     private async Task ScrollToAnchorAsync(string? inputLocation)
     {
+        // Initialize the location based on the input parameter.
         string location;
         if (inputLocation is null)
         {
+            // If 'inputLocation' was null, then set the location to the current
+            // Uri reported by 'NavigationManager'.
             location = NavigationManager.Uri;
         }
         else
         {
+            // Otherwise, set the location the provided input.
             location = inputLocation;
         }
 
+        // Run a regex match on the location.
         Match anchorTagMatch = _anchorTagRegex.Match(location);
 
+        // If the regex match was successful and the 'anchorTagName' group's value is not null,
+        // then attempt to scroll to the element.
         if (anchorTagMatch.Success == true && anchorTagMatch.Groups["anchorTagName"].Value is not null)
         {
+            /*
+                Loop for a max of 4 times while attempting to scroll.
+                We don't want to have this go on infinitely, so we're going to cap it at 4 times.
+                The reason for doing this is because of a limitation of where we're handling the logic.
+                If the logic was handled on the pages, we would have to define the logic on each page.
+                Doing it on the main layout that handles the routing between pages, allows us to define it
+                once; however, the main layout has no idea if the page has finished loading. This causes the
+                initial attempt to potentially fail because the specified element doesn't exist at that point in time.
+            */
+
             bool scrollWasSuccessful = false;
             for (int i = 1; i < 5 && scrollWasSuccessful == false; i++)
             {
                 try
                 {
+                    // Attempt to scroll to the element specified in the anchor using JavaScript interop.
                     await _mainLayoutJSModule!.InvokeVoidAsync("scrollToAnchorId", anchorTagMatch.Groups["anchorTagName"].Value);
+
+                    // If no exception was thrown, then set 'scrollWasSuccessful' to true to end the loop early.
                     scrollWasSuccessful = true;
                 }
                 catch (JSException e)
                 {
-                    Logger.LogInformation("'{ErrorMessage}' was thrown during loop {LoopCount}.", e.Message, i);
+                    // If an exception was thrown while executing the JS, then attemp to log a warning message
+                    // and wait 1000ms (1s) to try again.
+                    Logger.LogWarning("'{ErrorMessage}' was thrown during loop {LoopCount}.", e.Message, i);
                     await Task.Delay(1000);
                 }
             }
