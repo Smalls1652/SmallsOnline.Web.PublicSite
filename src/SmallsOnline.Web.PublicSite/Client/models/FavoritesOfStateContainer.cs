592c9274ae7b743e92e6f89347970da452dd3572
Timothy Small <smalls.online@icloud.com>
2022-11-01 16:50:20 -0400

    Added FavoritesOfStateContainer

---
+++ src/SmallsOnline.Web.PublicSite/Client/models/FavoritesOfStateContainer.cs
@@ -0,0 +1,19 @@
+namespace SmallsOnline.Web.PublicSite.Client.Models;
+
+public class FavoritesOfStateContainer
+{
+    private string? _savedListYear;
+    public string? ListYear
+    {
+        get => _savedListYear;
+        set
+        {
+            _savedListYear = value;
+            NotifyStateChanged();
+        }
+    }
+
+    public event Action? OnChange;
+
+    private void NotifyStateChanged() => OnChange?.Invoke();
+}
